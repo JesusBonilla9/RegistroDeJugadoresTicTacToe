27267e33c1d9fe2952a87c411ee121a32de4583f
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2025-09-13 17:00:09 -0400

    se corrigio una parte de la organizacion del codigo

--- RegistroDeJugadoresTicTacToe/Services/PartidasService.cs
+++ RegistroDeJugadoresTicTacToe/Services/PartidasService.cs
@@ -59,6 +59,9 @@ public class PartidasService(IDbContextFactory<Contexto> DbFactory)
     public async Task<List<Partidas>> Listar(Expression<Func<Partidas, bool>> criterio)
     {
         await using var contexto = await DbFactory.CreateDbContextAsync();
-        return await contexto.Partidas.Where(criterio).AsNoTracking().ToListAsync();
+        return await contexto.Partidas
+            .Where(criterio)
+            .AsNoTracking()
+            .ToListAsync();
     }
 }
