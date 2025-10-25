9193aeb3955ad6b697b7b2ffcd95e43841fe08a9
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2025-10-25 12:26:53 -0400

    Se creo el proyecto de tipo Libreria de clases Shared y Resource

---
+++ RegistroDeJugadoresTicTacToe.Shared/DTOs/MovimientoResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroDeJugadoresTicTacToe.Shared.DTOs;
+
+public record MovimientoResponse(
+    int MovimientoId,
+    int PartidaId,
+    int JugadorId,
+    int PosicionColumna,
+    int PosicionFila
+);
