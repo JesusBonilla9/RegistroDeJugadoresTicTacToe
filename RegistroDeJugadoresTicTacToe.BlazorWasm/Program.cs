9b1ed076eb04618de9091f9c4fc32baa0024e7a9
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2025-10-25 12:30:01 -0400

    Se hicieron cambios en el program y se inyectaron los imports en imports.razor

--- RegistroDeJugadoresTicTacToe.BlazorWasm/Program.cs
+++ RegistroDeJugadoresTicTacToe.BlazorWasm/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(https://gestionhuacalesapi.azurewebsites.net/) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://gestionhuacalesapi.azurewebsites.net/") });
 
 await builder.Build().RunAsync();
