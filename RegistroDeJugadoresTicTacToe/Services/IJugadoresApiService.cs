7810808f7f55250306458a378b0ad124b09438aa
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2025-10-26 14:43:21 -0400

    Creacion de las interfaces IMovimientosApiService y IJugadoresApiService

---
+++ RegistroDeJugadoresTicTacToe/Services/IJugadoresApiService.cs
@@ -0,0 +1,12 @@
+﻿using RegistroDeJugadoresTicTacToe.Shared;
+using RegistroDeJugadoresTicTacToe.Shared.DTOs;
+
+namespace RegistroDeJugadoresTicTacToe.Services
+{
+    public interface IJugadoresApiService
+    {
+        Task<Resource<List<JugadorResponse>>> GetJugadoresAsync();
+        Task<Resource<JugadorResponse>> GetJugadorAsync(int jugadorId);
+        Task<Resource<JugadorResponse>> PostJugador(string nombres, string email);
+    }
+}
---
+++ RegistroDeJugadoresTicTacToe/Services/IMovimientosApiService.cs
@@ -0,0 +1,11 @@
+﻿using RegistroDeJugadoresTicTacToe.Shared;
+using RegistroDeJugadoresTicTacToe.Shared.DTOs;
+
+namespace RegistroDeJugadoresTicTacToe.Services
+{
+    public interface IMovimientosApiService
+    {
+        Task<Resource<MovimientoResponse>> GetMovimientoAsync(int partidaId);
+        Task<Resource<MovimientoResponse>> PostMovimiento(int PartidaId, string Jugador, int PosicionFila, int PosicionColumna);
+    }
+}
