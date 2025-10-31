0526e610c593b775bdc499c48899cda0869f7bbc
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2025-10-31 17:43:55 -0400

    Se movio y adapto la carpeta DTOs del proyecto wasm a mi proyecto

---
+++ RegistroDeJugadoresTicTacToe/DTOs/PartidaRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroDeJugadoresTicTacToe.DTOs;
+
+public record PartidaRequest(
+    int Jugador1Id,
+    int Jugador2Id
+);
