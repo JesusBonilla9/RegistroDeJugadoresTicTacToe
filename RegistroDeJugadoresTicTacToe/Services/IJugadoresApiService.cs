a26150b3b1a45c5cb718cb5304304287842f10b8
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2025-10-31 17:45:56 -0400

    Se movio y adapto la carpeta Services a mi programa

---
+++ RegistroDeJugadoresTicTacToe/Services/IJugadoresApiService.cs
@@ -0,0 +1,12 @@
+﻿
+using RegistroDeJugadoresTicTacToe.DTOs;
+
+namespace RegistroDeJugadoresTicTacToe.Services;
+
+public interface IJugadoresApiService
+{
+    Task<Resource<List<JugadorResponse>>> GetJugadoresAsync();
+    Task<Resource<JugadorResponse>> GetJugadoresAsync(int JugadorId);
+    Task<Resource<JugadorResponse>> PostJugadores(string nombre, string email);
+    Task<Resource<JugadorResponse>> PutJugador(int JugadorId, string nombre, string email);
+}
---
+++ RegistroDeJugadoresTicTacToe/Services/IPartidaApiService.cs
@@ -0,0 +1,11 @@
+﻿using RegistroDeJugadoresTicTacToe.DTOs;
+using RegistroDeJugadoresTicTacToe;
+
+namespace RegistroDeJugadoresTicTacToe.Services;
+
+public interface IPartidaApiService
+{
+    Task<Resource<List<PartidaResponse>>> GetPartidasAsync();
+    Task<Resource<PartidaResponse>> GetPartidaAsync(int partidaId);
+    Task<Resource<PartidaResponse>> PostPartida(int jugador1, int jugador2);
+}
